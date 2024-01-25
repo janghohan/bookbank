f0c10eb1328a27a88739793a803febee1be0e63d
janghohan <wkdgh5430@naver.com>
2024-01-25 14:47:58 +0900

    detail 작업

---
+++ API/cart_create_api.php
@@ -0,0 +1,40 @@
+<?php
+include '../dbConnect.php';
+
+$user_ix = isset($_COOKIE['user_ix']) ? $_COOKIE['user_ix'] : 0;
+
+$bookIx = isset($_POST['bookIx']) ? $_POST['bookIx'] : 1;
+
+
+$chkSql = "SELECT * FROM cart WHERE user_ix='$user_ix' AND book_ix='$bookIx'";
+$chkResult = $conn->query($chkSql);
+$chkCount = $chkResult->num_rows;
+
+if($chkCount <= 0){
+	$sql = "INSERT INTO cart(user_ix,book_ix,quantity,rental_price) SELECT $user_ix, $bookIx, 1, price FROM books WHERE book_ix='$bookIx'";
+
+	if($conn->query($sql)){
+		$data = json_encode(array(
+	        'resultCode' => 1
+	    ));
+	}else{
+		$data = json_encode(array(
+	        'resultCode' => 0
+	    ));
+	}
+}else{
+	//이미 장바구니에 담긴상품이라면 수량 늘리기
+	// $sql = "UPDATE cart SET quantity = quantity + 1 WHERE user_ix='$user_ix' AND book_ix='$bookIx'";
+
+	$data = json_encode(array(
+        'resultCode' => -1
+    ));
+}
+
+
+echo $data;
+
+$conn->close();
+
+
+?>
