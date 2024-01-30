ca695967ba7ba1dc079b2bc460bcaa7e8307252b
janghohan <wkdgh5430@naver.com>
2024-01-30 13:39:17 +0900

    내 정보 관리 완료

--- API/info_edit_api.php
+++ API/info_edit_api.php
@@ -6,7 +6,7 @@ $expiration_time = time() + (86400 * 30);
 
 
 $editId = isset($_POST['edit_id']) ? $_POST['edit_id'] : 'wkdgh5430';
-$editEmail = isset($_POST['edit_email']) ? $_POST['edit_email'] : 'wkdgh5430';
+$editName = isset($_POST['edit_name']) ? $_POST['edit_name'] : 'wkdgh5430';
 $editContact = isset($_POST['edit_contact']) ? $_POST['edit_contact'] : 'wkdgh4232';
 
 
--- API/pwd_edit_api.php
+++ API/pwd_edit_api.php
@@ -1,7 +1,7 @@
 <?php 
 include '../dbConnect.php';
 
-$userId = isset($_POST['user_id']) ? $_POST['user_id'] : '';
+$userIx = isset($_COOKIE['user_ix']) ? $_COOKIE['user_ix'] : '';
 $exPassword = isset($_POST['ex_password']) ? $_POST['ex_password'] : '';
 $newPassword = isset($_POST['new_password']) ? $_POST['new_password'] : '';
 $newPassword2 = isset($_POST['new_password2']) ? $_POST['new_password2'] : '';
@@ -9,16 +9,16 @@ $newPassword2 = isset($_POST['new_password2']) ? $_POST['new_password2'] : '';
 $hashedPassword = md5($newPassword);
 
 //기존 비밀번호 체크
-$chk_sql = "SELECT * FROM user_list WHERE id='$userId'";
+$chk_sql = "SELECT * FROM user WHERE user_ix='$userIx'";
 $result = $conn->query($chk_sql);
 $row = $result->fetch_assoc();
 
-if(md5($exPassword) != $row['pwd']) {
+if(md5($exPassword) != $row['user_pwd']) {
     echo '기존 비밀번호가 일치하지 않습니다.';
 }else if($newPassword!=$newPassword2) {
     echo '새 비밀번호가 일치하지 않습니다.';
 }else{
-    $sql = "UPDATE user_list SET pwd='$hashedPassword' WHERE id='$userId'";
+    $sql = "UPDATE user SET user_pwd='$hashedPassword' WHERE user_ix='$userIx'";
 
     if($conn->query($sql)){
         echo 1;
